c18fa66f95dafe3acd87792503fe12bb862bdeaa
dan list <listda7921@gmail.com>
2017-06-01 17:21:24 -0500

    dropdowns

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,6 +1,7 @@
-app.controller('View1Ctrl', function($scope,$document) {
-
-  $scope.scroll = function(position){
+app.controller('View1Ctrl', function($scope, $document) {
+  $scope.text = 'more';
+  $scope.text2 = 'more';
+  $scope.scroll = function(position) {
     var duration = 700;
     var offset = 0;
     var element = angular.element(document.getElementById(position));
