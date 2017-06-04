aeb3587652d1880ec1d07036e82261772fb76550
dan <listda7921@gmail.com>
2017-06-04 14:55:18 -0500

    experience info

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,6 +1,8 @@
 app.controller('View1Ctrl', function($scope, $document) {
   $scope.text = 'more';
   $scope.text2 = 'more';
+  $scope.text3 = 'more';
+  $scope.text4 = 'more';
   $scope.scroll = function(position) {
     var duration = 700;
     var offset = 0;
