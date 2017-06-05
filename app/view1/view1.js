a63dbbd35548637a84e041d745b77e38b96b31dc
dan <listda7921@gmail.com>
2017-06-05 16:00:43 -0500

    skill gauges

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -12,37 +12,97 @@ app.controller('View1Ctrl', function($scope, $document) {
   }
 
   $scope.skills = [
-    'C#',
-    'Javascript',
-    'Angular Js',
-    'Angular 2',
-    'HTML5',
-    'CSS3',
-    'Bower',
-    'Npm',
-    'AWS',
-    'JQuery',
-    'Bootstrap',
-    'ASP.NET',
-    'LINQ',
-    'SQL',
-    'Node.js',
-    'Ionic Framework',
-    'Git',
-  ];
-  $scope.filteredSkills = $scope.skills;
-  $scope.skillChange = function(skill) {
-    if (skill === true) {
-      $scope.filteredSkills = [];
-      $scope.skills.forEach(function(skill) {
-        if (skill === 'C#' || skill === 'Javascript' || skill === 'Angular Js' || skill === 'HTML5' || skill === 'CSS3') {
-          $scope.filteredSkills.push(skill);
-        }
-      })
-    } else if (skill === false) {
-      $scope.filteredSkills = $scope.skills;
-    }
+  {
+  	skill: 'C#',
+  	value: 4
+  },
+  {
+  	skill: 'Javascript',
+  	value: 4
+  },
+  {
+  	skill: 'Angular Js',
+  	value: 4
+  },
+  {
+  	skill: 'Angular 2',
+  	value: 3
+  },
+  {
+  	skill: 'HTML5',
+  	value: 5
+  },
+  {
+  	skill: 'CSS3',
+  	value: 4
+  },
+  {
+  	skill: 'Bower/Npm',
+  	value: 5
+  },
+  {
+  	skill: 'AWS',
+  	value: 2
+  },
+  {
+  	skill: 'JQuery',
+  	value: 3
+  },
+  {
+  	skill: 'Bootstrap',
+  	value: 4
+  },
+  {
+  	skill: 'ASP.NET',
+  	value: 3
+  },
+  {
+  	skill: 'LINQ',
+  	value: 3
+  },
+  {
+  	skill: 'SQL',
+  	value: 2
+  },
+  {
+  	skill: 'Node.js',
+  	value: 4
+  },
+  {
+  	skill: 'Ionic Framework',
+  	value: 4
+  },
+  {
+  	skill: 'Git',
+  	value: 5
   }
-  $scope.skillChange(false);
+  ];
+  $scope.sort = function(val){
+  	if(val == true){
+  $scope.skills = $scope.skills.sort(function(a,b){
+  	return b.value - a.value;
+  });
+}
+  else{
+  	$scope.skills = $scope.skills.sort(function(a,b){
+  	return b.skill - a.skill;
+  });
+  }
+
+  }
+  // $scope.filteredSkills = $scope.skills;
+  // $scope.skillChange = function(skill) {
+  //   if (skill === true) {
+  //     $scope.filteredSkills = [];
+  //     $scope.skills.forEach(function(skill) {
+  //       if (skill === 'C#' || skill === 'Javascript' || skill === 'Angular Js' || skill === 'HTML5' || skill === 'CSS3') {
+  //         $scope.filteredSkills.push(skill);
+  //       }
+  //     })
+  //   } else if (skill === false) {
+  //     $scope.filteredSkills = $scope.skills;
+  //   }
+  // }
+  // $scope.skillChange(false);
 
 });
