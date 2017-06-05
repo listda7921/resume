f8432fe11955d6aad416493b6bc14ec643fcf424
dan list <listda7921@gmail.com>
2017-06-05 16:24:34 -0500

    skills

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -11,85 +11,72 @@ app.controller('View1Ctrl', function($scope, $document) {
     $document.scrollTo(element, offset, duration);
   }
 
-  $scope.skills = [
-  {
-  	skill: 'C#',
-  	value: 4
-  },
-  {
-  	skill: 'Javascript',
-  	value: 4
-  },
-  {
-  	skill: 'Angular Js',
-  	value: 4
-  },
-  {
-  	skill: 'Angular 2',
-  	value: 3
-  },
-  {
-  	skill: 'HTML5',
-  	value: 5
-  },
-  {
-  	skill: 'CSS3',
-  	value: 4
-  },
-  {
-  	skill: 'Bower/Npm',
-  	value: 5
-  },
-  {
-  	skill: 'AWS',
-  	value: 2
-  },
-  {
-  	skill: 'JQuery',
-  	value: 3
-  },
-  {
-  	skill: 'Bootstrap',
-  	value: 4
-  },
-  {
-  	skill: 'ASP.NET',
-  	value: 3
-  },
-  {
-  	skill: 'LINQ',
-  	value: 3
-  },
-  {
-  	skill: 'SQL',
-  	value: 2
-  },
-  {
-  	skill: 'Node.js',
-  	value: 4
-  },
-  {
-  	skill: 'Ionic Framework',
-  	value: 4
-  },
-  {
-  	skill: 'Git',
-  	value: 5
-  }
+  $scope.skills = [{
+      skill: 'C#',
+      value: 4
+    },
+    {
+      skill: 'Javascript',
+      value: 4
+    },
+    {
+      skill: 'Angular Js',
+      value: 4
+    },
+    {
+      skill: 'Angular 2',
+      value: 3
+    },
+    {
+      skill: 'HTML5',
+      value: 5
+    },
+    {
+      skill: 'CSS3',
+      value: 4
+    },
+    {
+      skill: 'Bower/Npm',
+      value: 5
+    },
+    {
+      skill: 'JQuery',
+      value: 3
+    },
+    {
+      skill: 'Bootstrap',
+      value: 4
+    },
+    {
+      skill: 'ASP.NET',
+      value: 3
+    },
+    {
+      skill: 'LINQ',
+      value: 3
+    },
+    {
+      skill: 'SQL',
+      value: 2
+    },
+    {
+      skill: 'Node.js',
+      value: 4
+    },
+    {
+      skill: 'Ionic Framework',
+      value: 4
+    },
+    {
+      skill: 'Git',
+      value: 5
+    }
   ];
-  $scope.sort = function(val){
-  	if(val == true){
-  $scope.skills = $scope.skills.sort(function(a,b){
-  	return b.value - a.value;
-  });
-}
-  else{
-  	$scope.skills = $scope.skills.sort(function(a,b){
-  	return b.skill - a.skill;
-  });
-  }
 
-  }
+  // $scope.skills = $scope.skills.sort(function(a, b) {
+  //   return b.value - a.value;
+  // });
+
   // $scope.filteredSkills = $scope.skills;
   // $scope.skillChange = function(skill) {
   //   if (skill === true) {
